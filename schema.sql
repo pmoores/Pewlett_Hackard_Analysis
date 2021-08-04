3483704f97245aec59cbcf2cbe40cc39305b39a2
Patrick Moores <patrickmoores@Patricks-MacBook-Air.local>
2021-08-03 20:07:49 -0400

    Update after 7.2.4

--- schema.sql
+++ schema.sql
@@ -40,14 +40,15 @@ CREATE TABLE salaries (
 
 -- Creating dept_emp table
 CREATE TABLE dept_emp (
-	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (dept_no)
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (dept_no, emp_no)
 );
+
 
 -- Creating titles table
 CREATE TABLE titles (
@@ -56,8 +57,11 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
 );
 
 -- Run a SELECT statement to query the database
-SELECT * FROM departments;
+SELECT * FROM titles;
+
+-- Delete table, fix errors in original table code, rerun code
+-- CASCADE removes connections to other tables in the database
+DROP TABLE dept_emp CASCADE;
