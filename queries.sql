79eee5c2818b9c9fdebe76762f6c44c296338e0d
Patrick Moores <patrickmoores@Patricks-MacBook-Air.local>
2021-08-05 17:52:31 -0400

    Update after 7.3.6

--- queries.sql
+++ queries.sql
@@ -152,6 +152,28 @@ INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no);
 
 
+-- 7.3.6 SKILL DRILL Tailor the department retirees information to 
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info_sales
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+AND (d.dept_name = 'Sales');
 
-
-
+-- SKILL DRILL Create the same tailored list but include the Sales and Development teams
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info_sales_devel
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales', 'Development');
