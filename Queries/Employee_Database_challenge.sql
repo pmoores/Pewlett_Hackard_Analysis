b1632ca41826382889f9ed6ab2522ef3a3884d86
Patrick Moores <patrickmoores@Patricks-MacBook-Air.local>
2021-08-12 20:25:04 -0400

    Update after help from TA

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -11,7 +11,7 @@ emp.last_name,
 	tt.to_date
 INTO retirement_titles
 FROM employees as emp
-LEFT JOIN titles as tt
+INNER JOIN titles as tt
 ON emp.emp_no = tt.emp_no
 WHERE (emp.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no;
@@ -23,18 +23,17 @@ last_name,
 title
 INTO unique_titles
 FROM retirement_titles
-ORDER BY emp_no, to_date ASC;
+ORDER BY emp_no ASC, to_date DESC;
+
 
 -- 1.15 to 1.18 Retrieve the number of employees who are about to retire 
 -- by their most recent job title
+-- Solution via discussion with TA Sunny Patel on Slack Aug 12-2021
 SELECT COUNT(title), title
--- INTO retiring_titles
+INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
-
-SELECT * FROM retiring_titles
-DROP TABLE retiring_titles
 
 -- DELIVERABLE 2 - Employees Eligible for the Mentorship Program
 -- 2.1 to 2.9 Create a new table that displays mentorship eligibility
@@ -44,13 +43,13 @@ emp.last_name,
 emp.birth_date,
 de.from_date,
 de.to_date,
-tit.title
+tt.title
 INTO mentorship_eligibility
 FROM employees as emp
 LEFT JOIN dept_emp as de
 ON emp.emp_no = de.emp_no
-LEFT JOIN titles as tit
-ON de.emp_no = tit.emp_no
+LEFT JOIN titles as tt
+ON de.emp_no = tt.emp_no
 WHERE de.to_date = ('9999-01-01')
 AND (emp.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY emp.emp_no ASC
